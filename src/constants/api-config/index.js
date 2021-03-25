ba8f922bba0007362205a3fa05bc335725a50894
hemantkumar2 <hemantnirmalkar17@gmail.com>
2021-03-26 00:24:26 +0530

    added axios, called nasa api and displayed picture of the day

--- src/constants/api-config/index.js
+++ src/constants/api-config/index.js
@@ -1,1 +1,2 @@
 export const API_ROOT = "https://api.nasa.gov/";
+export const API_KEY = "dtvmMT0bEhc8HcBoiuResI1bB2vlFnxPjwQRsKH8";
--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { API_ROOT, API_KEY } from "constants/api-config";
+import { Input, Button, Form } from "antd";
 
 const index = () => {
-  return <div>NASA Picture of the day</div>;
+  const [apodData, setApodData] = useState(null);
+  useEffect(async () => {
+    const apodData = await axios.get(
+      `${API_ROOT}planetary/apod?api_key=${API_KEY}`
+    );
+    const { data } = apodData;
+    setApodData(data);
+  }, []);
+
+  return (
+    <>
+      {apodData && (
+        <div
+          style={{
+            paddingTop: "30vh",
+            textAlign: "center",
+          }}
+        >
+          <h1>NASA Media search</h1>
+          <Form>
+            <Input style={{ width: "20rem" }} />
+            <Button>Search</Button>
+          </Form>
+          <br />
+
+          <img
+            style={{ width: "50vh" }}
+            src={apodData.url}
+            alt={apodData.title}
+          />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default index;
