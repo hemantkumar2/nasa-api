8ba3d9d0377526548224adc5c1dafe55105abf48
hemantkumar2 <hemantnirmalkar17@gmail.com>
2021-03-29 18:03:45 +0530

    handling search input text and pusing search-results to history on search btn click

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 import { API_ROOT, API_KEY } from "constants/api-config";
 import { Input, Button, Form } from "antd";
+import { SearchContext } from "context/SearchContext";
 
 const index = () => {
   const [apodData, setApodData] = useState(null);
+  const [searchInputText, setSearchInputText] = useState("");
+  const { setSearchText } = useContext(SearchContext);
+  const history = useHistory();
   useEffect(async () => {
     const apodData = await axios.get(
       `${API_ROOT}planetary/apod?api_key=${API_KEY}`
@@ -12,6 +17,14 @@ const index = () => {
     const { data } = apodData;
     setApodData(data);
   }, []);
+
+  const handleInputChange = (e) => {
+    setSearchInputText(e.target.value);
+  };
+  const handleSearch = () => {
+    setSearchText(searchInputText);
+    history.push("/search-results");
+  };
 
   return (
     <>
@@ -24,8 +37,11 @@ const index = () => {
         >
           <h1>NASA Media search</h1>
           <Form>
-            <Input style={{ width: "20rem" }} />
-            <Button>Search</Button>
+            <Input
+              onChange={(e) => handleInputChange(e)}
+              style={{ width: "20rem" }}
+            />
+            <Button onClick={handleSearch}>Search</Button>
           </Form>
           <br />
 
