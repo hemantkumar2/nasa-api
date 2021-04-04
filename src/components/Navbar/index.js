1a252b8388ac146066bd3cacc44a00f64a2ce76e
hemantkumar2 <hemantnirmalkar17@gmail.com>
2021-04-04 12:31:42 +0530

    updated navbar with icon

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,19 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { Row, Col } from "antd";
+import NasaSvg from "Assets/svgs/nasa.svg";
 
 import "./index.scss";
 
 const Navbar = (props) => {
   return (
     <Row className="navbar">
-      <Col span={24}>{props.children}</Col>
+      <Col span={24}>
+        <div className="navbar-logo">
+          <object data={NasaSvg} width="50" height="50"></object>
+        </div>
+        {props.children}
+      </Col>
     </Row>
   );
 };
