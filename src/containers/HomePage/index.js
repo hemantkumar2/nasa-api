99cb11ed14e35fb5b0628571821d1c0820471172
hemantkumar2 <hemantnirmalkar17@gmail.com>
2021-04-04 14:43:23 +0530

    code refactoring and added loader

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
-import { Form, message, Card, Row, Col } from "antd";
+import { Form, message, Card, Row, Col, Spin } from "antd";
 import { useHistory } from "react-router-dom";
 
 import { API_ROOT_APOD } from "constants/api-config";
@@ -13,7 +13,7 @@ import "./index.scss";
 
 const { REACT_APP_API_KEY } = process.env;
 const index = () => {
-  const [apodData, setApodData] = useState({});
+  const [apodData, setApodData] = useState();
   const [searchInputText, setSearchInputText] = useState("");
   const { setSearchText } = useContext(SearchContext);
   const history = useHistory();
@@ -38,27 +38,23 @@ const index = () => {
 
   const getApod = () => {
     return (
-      <>
-        {apodData && (
-          <div>
-            <Row justify="center">
-              <Col span={24}>
-                <img
-                  className="apod-image"
-                  src={apodData.url}
-                  alt={apodData.title}
-                />
-                <div className="apod-details-container">
-                  <h2>{apodData.title}</h2>
-                  <p className="apod-date">{apodData.date}</p>
-                  <p className="apod-explanation">{apodData.explanation}</p>
-                </div>
-              </Col>
-            </Row>
-            <br />
-          </div>
-        )}
-      </>
+      <div>
+        <Row justify="center">
+          <Col span={24}>
+            <img
+              className="apod-image"
+              src={apodData.url}
+              alt={apodData.title}
+            />
+            <div className="apod-details-container">
+              <h2>{apodData.title}</h2>
+              <p className="apod-date">{apodData.date}</p>
+              <p className="apod-explanation">{apodData.explanation}</p>
+            </div>
+          </Col>
+        </Row>
+        <br />
+      </div>
     );
   };
 
@@ -89,6 +85,21 @@ const index = () => {
       </Navbar>
     );
   };
+  const getHero = () => {
+    if (!apodData)
+      return (
+        <div className="loader">
+          <Spin size="large" />
+        </div>
+      );
+    return (
+      <Row justify="center">
+        <Col xs={22} md={18} lg={14}>
+          <Card className="apod-card">{getApod()}</Card>
+        </Col>
+      </Row>
+    );
+  };
 
   return (
     <>
@@ -99,11 +110,7 @@ const index = () => {
       >
         NASA Media
       </h1>
-      <Row justify="center">
-        <Col xs={22} md={18} lg={14}>
-          <Card className="apod-card">{getApod()}</Card>
-        </Col>
-      </Row>
+      {getHero()}
     </>
   );
 };
